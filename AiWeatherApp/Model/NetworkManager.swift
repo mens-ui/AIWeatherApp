77da97440e2d3a6d1aa71d42c1db2ae0c2d55df4
dbsghdz1 <dbsghdz1@gmail.com>
2024-07-20 23:12:02 +0900

    Fix: 날짜형식

--- AiWeatherApp/Model/NetworkManager.swift
+++ AiWeatherApp/Model/NetworkManager.swift
@@ -6,14 +6,16 @@
 //
 
 import Foundation
+import UIKit
 
 class NetworkManager {
   
   static let shared = NetworkManager()
+  let location = Data()
   
   func getCurrentData(_ completion: @escaping (CurrentWeatherResult?) -> Void) {
     guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "OPEN_WEATHER_API_KEY") as? String else { return }
-    let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=37.56&lon=125.97&units=metric&appid=\(apiKey)&lang=kr")!
+    let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(location.lat)&lon=\(location.lon)&units=metric&appid=\(apiKey)&lang=kr")!
     
     let task = URLSession.shared.dataTask(with: url) { data, response, error in
       guard let data = data, error == nil else {
@@ -33,7 +35,7 @@ class NetworkManager {
   
   func getWeekData(_ completion: @escaping (WeekWeatherResult?) -> Void) {
     guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "OPEN_WEATHER_API_KEY") as? String else { return }
-    let url = URL(string: "https://api.openweathermap.org/data/2.5/forecast?lat=37.56&lon=125.97&units=metric&appid=\(apiKey)&lang=kr")!
+    let url = URL(string: "https://api.openweathermap.org/data/2.5/forecast?lat=\(location.lat)&lon=\(location.lon)&units=metric&appid=\(apiKey)&lang=kr")!
     
     let task = URLSession.shared.dataTask(with: url) { data, response, error in
       guard let data = data, error == nil else {
@@ -49,5 +51,26 @@ class NetworkManager {
       }
     }
     task.resume()
+  }
+  
+  func getIcon(_ completion: @escaping (UIImage?) -> Void) {
+    getWeekData { weatherData in
+      guard let weatherData = weatherData,
+            let icon = weatherData.list.first?.weather.first?.icon,
+            let imageUrl = URL(string: "https://openweathermap.org/img/wn/\(icon)@2x.png") else {
+        completion(nil)
+        return
+      }
+      
+      let task = URLSession.shared.dataTask(with: imageUrl) { data, response, error in
+        guard let data = data, error == nil, let image = UIImage(data: data) else {
+          print("Error downloading image: \(error?.localizedDescription ?? "Unknown error")")
+          completion(nil)
+          return
+        }
+        completion(image)
+      }
+      task.resume()
+    }
   }
 }
